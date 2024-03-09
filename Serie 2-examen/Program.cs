bc4a3272b34bae7c3f4cbe1bc1d05b48892e8b31
Yessenia <106893050+Yessi0908@users.noreply.github.com>
2024-03-09 12:50:39 -0600

    Add files via upload

---
+++ Serie 2-examen/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serie_2_examen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido, ingrese un correo con dominio de Gmail por favor");
+            string cgmail = Console.ReadLine();
+
+           if (cgmail.EndsWith("gmail.com"))
+            {
+                string cndom = cgmail.Replace("gmail.com", "miumg.edu.gt");
+                Console.WriteLine("Su nuevo dominio es: " +  cndom);
+
+            }
+            else
+            {
+                Console.WriteLine("Verificar si el dominio es de Gmail"); 
+            }
+           
+        }
+
+    }
+}
